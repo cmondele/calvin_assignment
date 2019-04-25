431bb7d7c8570120c1d77b1efa701ffef1f51e47
calvin mondele <cmondele88@gmail.com>
2019-04-25 17:47:02 -0600

    cool

---
+++ exercises/protocole/server.js
@@ -0,0 +1,11 @@
+const express = required('express')
+const mongoose = require('mongosse')
+
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`)
+});
